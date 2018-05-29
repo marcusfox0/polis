dashpay/dash-dev-branches

Automatically lock ProTx collaterals when TX is added/loaded to wallet
Avoid accidential spending of ProTx collaterals.

src/evo/providertx.cpp
@@ -106,3 +106,16 @@ void CProviderTXRegisterMN::ToJson(UniValue &obj) const {
     obj.push_back(Pair("payout", payoutObj));
     obj.push_back(Pair("inputsHash", inputsHash.ToString()));
 }
+
+bool IsProTxCollateral(const CTransaction &tx, int n) {
+    return GetProTxCollateralIndex(tx) == n;
+}
+
+int GetProTxCollateralIndex(const CTransaction &tx) {
+    if (tx.nVersion < 3 || tx.nType != TRANSACTION_PROVIDER_REGISTER)
+        return -1;
+    CProviderTXRegisterMN proTx;
+    if (!GetTxPayload(tx, proTx))
+        assert(false);
+    return proTx.nCollateralIndex;
+}

marcusfox0/polis

src/evo/providertx.cpp
@@ -124,3 +124,18 @@ void CProRegTx::ToJson(UniValue& obj) const
 
     obj.push_back(Pair("inputsHash", inputsHash.ToString()));
 }
+
+bool IsProTxCollateral(const CTransaction& tx, uint32_t n)
+{
+    return GetProTxCollateralIndex(tx) == n;
+}
+
+uint32_t GetProTxCollateralIndex(const CTransaction& tx)
+{
+    if (tx.nVersion < 3 || tx.nType != TRANSACTION_PROVIDER_REGISTER)
+        return (uint32_t) - 1;
+    CProRegTx proTx;
+    if (!GetTxPayload(tx, proTx))
+        assert(false);
+    return proTx.nCollateralIndex;
+}